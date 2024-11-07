0270bbdfa581dd204dd28217c1cbba2108a04c57
Jiyong Kang <kangz12345@snu.ac.kr>
2024-11-07 21:48:44 +0900

    Add routes in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import TodoList from "./containers/TodoList/TodoList";
+import NewTodo from "./containers/TodoList/NewTodo/NewTodo";
 
 function App() {
-  return <div className="App"><TodoList title={"My TODOs!"} /></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="todos" element={<TodoList title={"My TODOs!"} />} />
+          <Route path="new-todo" element={<NewTodo />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
