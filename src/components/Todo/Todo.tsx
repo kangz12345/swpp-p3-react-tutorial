bc3e0475008c61a4b5d9001de971475d00e8ecb1
Jiyong Kang <kangz12345@snu.ac.kr>
2024-11-07 20:54:02 +0900

    Implement `Todo` in components

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -3,3 +3,15 @@ interface IProps {
     clicked?: React.MouseEventHandler<HTMLDivElement>;
     done: boolean;
 }
+
+const Todo = (props: IProps) => {
+    return (
+        <div className="Todo">
+            <div className={`text ${props.done && "done"}`} onClick={props.clicked}>
+                {props.title}
+            </div>
+            {props.done && <div className="done-mark">&#x2713;</div>}
+        </div>
+    );
+};
+export default Todo;
