87ec11d32cbbb2ab5cb859ccadd4c5173619b621
Jiyong Kang <kangz12345@snu.ac.kr>
2024-11-07 20:55:17 +0900

    Add TodoList items

--- src/containers/TodoList/TodoList.tsx
+++ src/containers/TodoList/TodoList.tsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
+
 interface IProps {
     title: string;
 }
 
+type TodoType = {
+    id: number; title: string; content: string; done: boolean;
+};
+
 export default function TodoList(props: IProps) {
     const { title } = props;
+    const [todos, setTodos] = useState<TodoType[]>([
+        { id: 1, title: "SWPP", content: "take swpp class", done: true },
+        { id: 2, title: "Movie", content: "watch movie", done: false },
+        { id: 3, title: "Dinner", content: "eat dinner", done: false },
+    ])
     return <div className="TodoList">{title}</div>;
 }
