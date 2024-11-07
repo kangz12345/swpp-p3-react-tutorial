804c5ab6514861229602e7d398b9a44735b2e429
Jiyong Kang <kangz12345@snu.ac.kr>
2024-11-07 21:56:31 +0900

    Add alias redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import TodoList from "./containers/TodoList/TodoList";
 import NewTodo from "./containers/TodoList/NewTodo/NewTodo";
@@ -8,8 +8,9 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="todos" element={<TodoList title={"My TODOs!"} />} />
-          <Route path="new-todo" element={<NewTodo />} />
+          <Route path="/todos" element={<TodoList title={"My TODOs!"} />} />
+          <Route path="/new-todo" element={<NewTodo />} />
+          <Route path="/" element={<Navigate replace to={"/todos"} />} />
         </Routes>
       </BrowserRouter>
     </div>
